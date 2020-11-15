038301c5687e19fdaca98bf19950418debc58849
VKozlova90 <fancygirl90@mail.ru>
2020-11-15 14:46:05 +0300

    v.1.1

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,11 +1,33 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Person {
     private String firstName;
     private String lastName;
-    private Gender Gender;
+    private Gender gender;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return firstName.equals(person.firstName) &&
+                lastName.equals(person.lastName) &&
+                gender == person.gender;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, gender);
+    }
 
-
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
 }
